82fa459de9c321a24af822f5d1d5caadd64df6f6
DevinBayly <baylyd@arizona.edu>
2026-02-27 04:19:41 -0700

    working on the networking

---
+++ networking.gd
@@ -0,0 +1,60 @@
+extends Node
+signal received_user_id
+# By default, these expressions are interchangeable.
+var PORT = 8081
+#var IP_ADDRESS = "godotcommunicator.TRA220030.projects.jetstream-cloud.org"
+var IP_ADDRESS="localhost"
+var MAX_CLIENTS = 2
+var peer
+func _ready() -> void:
+	var arg = OS.get_cmdline_args()[0] # try to see if we started up with the word "server"
+	if arg == "server":
+		print("I am a server")
+		# Create server.
+		peer = ENetMultiplayerPeer.new()
+		peer.create_server(PORT, MAX_CLIENTS)
+		multiplayer.multiplayer_peer = peer
+		multiplayer.peer_connected.connect(server_handle_peer_connect)
+		
+	else:
+		print("I am a client")
+		var peer = ENetMultiplayerPeer.new()
+		peer.create_client(IP_ADDRESS, PORT)
+		multiplayer.multiplayer_peer = peer
+		multiplayer.connected_to_server.connect(client_connected_to_server)
+		multiplayer.connection_failed.connect(con_failed)
+		multiplayer.server_disconnected.connect(server_lost)
+func server_lost():
+	print("server lost")
+func con_failed():
+	print("failed to connect to server")
+func client_connected_to_server():
+	print("client made contact with server")
+func server_handle_peer_connect(id):
+	print("server was contacted by peer id",id)
+	
+# the goal will be to have function that a peer can start to signal to the server that the other peer needs some information
+@rpc("any_peer","call_remote","reliable",0)
+func send_user_id(other_user_id):
+	var sender_id = multiplayer.get_remote_sender_id()
+	# ensure it's not our own id, and that we aren't server, well I suppose since we called remote it wont be
+	if multiplayer.is_server():
+		pass
+	else:
+		# store this as other player's id
+		received_user_id.emit(other_user_id)
+
+@rpc("any_peer","call_remote","reliable",0)
+func send_anchor_uuid(shared_other_uuid):
+	var sender_id = multiplayer.get_remote_sender_id()
+	# ensure it's not our own id, and that we aren't server, well I suppose since we called remote it wont be
+	if multiplayer.is_server():
+		pass
+	else:
+		# store this as other player's id
+		received_user_id.emit(shared_other_uuid)
+
+
+# then we can have signals on this that say, received uuid anchor or user id is... 
+#
+#we will also maybe need to have something where the peer calls on the server a "vote" after placement, and the server registers that the vote matches a correct criteria?
